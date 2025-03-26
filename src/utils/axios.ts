dba86d52d0753c54ce5091842aa4ba0c42105c10
paulsaji200 <paulsaji1791@gmail.com>
2025-03-26 06:12:12 +0530

    Fixed API baseURL issue in axios requests2

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "ttps://dns-project-server.onrender.com/api/",
+  baseURL: "https://dns-project-server.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
